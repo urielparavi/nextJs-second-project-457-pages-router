3248af82c599dda3d245e32840e1944ef14c8f59
Your Name <urielparavi75@gmail.com>
2024-08-10 10:40:28 +0300

    the first branch

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://urielpa:urielpa30808@cluster0.7e0kg.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
